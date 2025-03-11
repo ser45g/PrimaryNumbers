bbd97d6952d59b871eed3b668783bf0b33e0f974
ser45g <sergey1pes@gmail.com>
2025-03-11 23:06:00 +0300

    Improve user interactions with the cmd interface

--- Program.cs
+++ Program.cs
@@ -2,30 +2,48 @@
 using System.Diagnostics;
 using System.Numerics;
 
+IPrimaryNumbersRangeFinder primaryNumbersRangeFinder;
 
+var action = (BigInteger i) => { Console.WriteLine("Prime number: " + i.ToString()); };
 
-IBigIntegerSqrtFinder bigIntegerSqrtFinder = new SteinerSqrtBigIntegerFinder();
-TrialPrimaryNumbersRangeFinder trialPrimaryNumbersRangeFinder = new TrialPrimaryNumbersRangeFinder(bigIntegerSqrtFinder);
+while (ConsoleKey.Enter!=Console.ReadKey().Key)
+{
+    Console.WriteLine("Enter the start value:");
+    string? startStr=Console.ReadLine();
 
-SieveOfEratosthenesPrimaryNumbersRangeFinder sieveOfEratosthenesPrimaryNumbersRangeFinder = new();
+    Console.WriteLine("Enter the end value:");
+    string? endStr=Console.ReadLine();
 
-Stopwatch stopwatch = Stopwatch.StartNew();
-BigInteger start = BigInteger.Parse("1000");
-BigInteger end = BigInteger.Parse("289000");
+    Console.WriteLine("Enter the algorithm you want to use (s for theh sieve of eraosthenes, t for the trial based one):");
+    char algorithmType=Console.ReadKey().KeyChar;
 
-var action=(BigInteger i) => { Console.WriteLine("Prime number: " + i.ToString()); };
-trialPrimaryNumbersRangeFinder.NextPrimaryNumberWasFound += action;
-//sieveOfEratosthenesPrimaryNumbersRangeFinder.NextPrimaryNumberWasFound += action;
+    if(algorithmType=='s' || algorithmType=='S')
+    {
+         primaryNumbersRangeFinder = new SieveOfEratosthenesPrimaryNumbersRangeFinder();
+        
 
-//IEnumerable<BigInteger> trialPrimaryNumbers = trialPrimaryNumbersRangeFinder.GetPrimaryNumbersInRange(start, end);
-IEnumerable<BigInteger> trialPrimaryNumbers = sieveOfEratosthenesPrimaryNumbersRangeFinder.GetPrimaryNumbersInRange(start, end);
+    }
+    else
+    {
+        IBigIntegerSqrtFinder bigIntegerSqrtFinder = new SteinerSqrtBigIntegerFinder();
+        primaryNumbersRangeFinder = new TrialPrimaryNumbersRangeFinder(bigIntegerSqrtFinder);
+    }
+    primaryNumbersRangeFinder.NextPrimaryNumberWasFound += action;
+    BigInteger start = BigInteger.Parse(startStr);
+    BigInteger end = BigInteger.Parse(endStr);
 
-trialPrimaryNumbersRangeFinder.NextPrimaryNumberWasFound-=action;
-sieveOfEratosthenesPrimaryNumbersRangeFinder.NextPrimaryNumberWasFound-=action;
-Console.WriteLine("*************************************");
-Console.WriteLine(trialPrimaryNumbers.Count());
-Console.WriteLine("*************************************");
-stopwatch.Stop();
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    var primaryNumbers=primaryNumbersRangeFinder.GetPrimaryNumbersInRange(start, end);
+    stopwatch.Stop();
 
-Console.WriteLine(stopwatch.ElapsedMilliseconds.ToString()+" ms ");
-Console.WriteLine((GC.GetAllocatedBytesForCurrentThread()/1024).ToString()+" kb");
+    Console.WriteLine("*************************************");
+    Console.WriteLine(primaryNumbers.Count());
+    Console.WriteLine("*************************************");
+
+    Console.WriteLine(stopwatch.ElapsedMilliseconds.ToString() + " ms ");
+    Console.WriteLine((GC.GetAllocatedBytesForCurrentThread() / 1024).ToString() + " kb");
+
+    primaryNumbersRangeFinder.NextPrimaryNumberWasFound -= action;
+
+}
+
